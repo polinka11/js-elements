b2baf143ed6420a899f1dc925ab118c0e7501646
Polina Cheban <polic.che.13@gmail.com>
2021-03-13 15:27:11 +0200

    повторив за вчителем

--- js/script.js
+++ js/script.js
@@ -1,2 +1,28 @@
 'use strict';
 
+const box = document.getElementsById('box'),
+    circles = document.getElementsByClassName('circle'),
+    btns = document.getElementsByTagName('button'),
+    oneHeart = document.querySelector('.heart.first');
+    hearts = document.querySelectorAll('.heart');
+
+console.log
+
+
+box.style.width = '500px';
+box.style.backgroundColor = 'orrange';
+
+btsn[2].classList.add('active');
+
+const div = document.createElement('div');
+div.classList.add('black');
+box.append(div);
+box.before(div);
+box.textContent = '<h1>Заголовок</h1>';
+
+
+
+
+
+
+
